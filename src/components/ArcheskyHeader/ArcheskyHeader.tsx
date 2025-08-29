cad77a4776599fa7e55a9c199c51864efd7f74e6
unknown <eowlfhd12@gmail.com>
2025-08-29 17:21:13 +0900

    Archesky 완성

--- src/components/ArcheskyHeader/ArcheskyHeader.tsx
+++ src/components/ArcheskyHeader/ArcheskyHeader.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './ArcheskyHeader.module.css';
 import menu from '../../assets/img/Menu1.svg';
 import { Link } from 'react-router-dom';
 
 function ArcheskyHeader() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <>
-      <header className={styles.archeskyHeader}>
+      <header className={`${styles.archeskyHeader} ${isScrolled ? styles.scrolled : ""}`}>
         <div>
           <img src={menu} alt='메뉴' />
           <h1>Archesky</h1>
